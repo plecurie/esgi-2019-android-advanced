11564c59f4462849b71848bacad2bfa1122f579c
Benjamin M <benjamin.maruani@devoteam.com>
2019-05-14 16:59:59 +0200

    [14/05/2019] - add recyclerView

--- app/src/main/java/com/maruani/esgi/androidadvanced/module/recyclerview/simplelist/SimpleListPresenter.kt
+++ app/src/main/java/com/maruani/esgi/androidadvanced/module/recyclerview/simplelist/SimpleListPresenter.kt
@@ -1,6 +1,20 @@
 package com.maruani.esgi.androidadvanced.module.recyclerview.simplelist
 
 import com.maruani.esgi.androidadvanced.module.common.BasePresenter
+import com.maruani.esgi.androidadvanced.module.recyclerview.simplelist.business.api.FlickrProvider
+import com.maruani.esgi.androidadvanced.module.recyclerview.simplelist.business.model.FlickrPhoto
 
 class SimpleListPresenter : BasePresenter<SimpleListView>() {
+
+    override fun onCreate() {
+        FlickrProvider.getPhotos(object : FlickrProvider.Listener<List<FlickrPhoto>> {
+            override fun onSuccess(data: List<FlickrPhoto>) {
+                view.updateData(data)
+            }
+
+            override fun onError(t: Throwable) {
+
+            }
+        })
+    }
 }
--- app/src/main/java/com/maruani/esgi/androidadvanced/module/recyclerview/simplelist/business/api/FlickrProvider.kt
+++ app/src/main/java/com/maruani/esgi/androidadvanced/module/recyclerview/simplelist/business/api/FlickrProvider.kt
@@ -1,16 +1,65 @@
 package com.maruani.esgi.androidadvanced.module.recyclerview.simplelist.business.api
 
 import com.maruani.esgi.androidadvanced.BuildConfig
+import com.maruani.esgi.androidadvanced.module.recyclerview.simplelist.business.dto.EPhotosResponse
+import com.maruani.esgi.androidadvanced.module.recyclerview.simplelist.business.dto.mapper.EPhotosResponseMapper
+import com.maruani.esgi.androidadvanced.module.recyclerview.simplelist.business.model.FlickrPhoto
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object FlickrProvider {
+    private val USER_ID = "94993041@N05"
     private var service: FlickrService
 
     init {
         service = Retrofit.Builder().baseUrl(BuildConfig.FLICKR_API_URL)
+            .client(createOkHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(FlickrService::class.java)
     }
+
+    private fun createOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor {
+                val request = it.request()
+                val url = request.url()
+
+                val urlBuilder = url.newBuilder()
+                    .addQueryParameter("api_key", BuildConfig.FLICKR_API_KEY)
+                    .addQueryParameter("format", BuildConfig.FLICKR_API_FORMAT)
+                    .addQueryParameter("nojsoncallback", BuildConfig.FLICKR_API_CALLBACK)
+
+                val newUrl = urlBuilder.build()
+                val newRequest = request.newBuilder().url(newUrl).build()
+                it.proceed(newRequest)
+            }
+            .build()
+    }
+
+    fun getPhotos(listener: Listener<List<FlickrPhoto>>) {
+        service.getPublicPhotos(USER_ID).enqueue(object : Callback<EPhotosResponse> {
+            override fun onFailure(call: Call<EPhotosResponse>, t: Throwable) {
+                listener.onError(t)
+            }
+
+            override fun onResponse(call: Call<EPhotosResponse>, response: Response<EPhotosResponse>) {
+                response.body()?.also {
+                    val photos = EPhotosResponseMapper().map(it)
+                    listener.onSuccess(photos)
+                }
+            }
+
+        })
+    }
+
+    interface Listener<T> {
+        fun onSuccess(data: T)
+        fun onError(t: Throwable)
+    }
+
 }
